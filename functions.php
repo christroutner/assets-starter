ebe01165ec55651e1c4c32876398e7468e40d979
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 15:58:04 -0700

    Adding function.php file

--- functions.php
+++ functions.php
@@ -4,10 +4,11 @@ echo 'Hello World!';
 
 
 // Includes
-
+include( get_template_directory() . '/includes/front/enqueue.php');
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
 
 // Shortcodes
 
+?>
