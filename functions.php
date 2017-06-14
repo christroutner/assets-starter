aef4e0e27c9a3e6053cd0d78a19374fe72ebe479
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 15:43:14 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+echo 'Hello World!';
+// Setup
+
+
+// Includes
+
+
+// Hooks
+add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
+
+// Shortcodes
+
---
+++ includes/front/enqueue.php
@@ -0,0 +1,5 @@
+<?php
+
+function ju_enqueue() {
+  
+}
