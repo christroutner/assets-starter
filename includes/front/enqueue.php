1ad298c4de9d74794540a1157d14b8e9ba4beb10
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 16:21:31 -0700

    Adding stylesheet php

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -3,26 +3,26 @@
 function ju_enqueue() {
   
   wp_register_style( 'ju_google_fonts', 'http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic' );
-  /*
-  wp_register_style( handle: 'ju_bootstrap', src: get_template_directory_uri() . '/assets/css/bootstrap.css');
-  wp_register_style( handle: 'ju_style', src: get_template_directory_uri() . '/assets/style.css');
-  wp_register_style( handle: 'ju_dark', src: get_template_directory_uri() . '/assets/css/dark.css');
-  wp_register_style( handle: 'ju_font_icons', src: get_template_directory_uri() . '/assets/css/font-icons.css');
-  wp_register_style( handle: 'ju_animate', src: get_template_directory_uri() . '/assets/css/animate.css');
-  wp_register_style( handle: 'ju_magnific_popup', src: get_template_directory_uri() . '/assets/css/magnific-popup.css');
-  wp_register_style( handle: 'ju_responsive', src: get_template_directory_uri() . '/assets/css/responsive.css');
-  wp_register_style( handle: 'ju_custom', src: get_template_directory_uri() . '/assets/css/custom.css');
   
-  wp_enqueue_style( handle: 'ju_google_fonts' );
-  wp_enqueue_style( handle: 'ju_bootstrap' );
-  wp_enqueue_style( handle: 'ju_style' );
-  wp_enqueue_style( handle: 'ju_dark' );
-  wp_enqueue_style( handle: 'ju_font_icons' );
-  wp_enqueue_style( handle: 'ju_animate' );
-  wp_enqueue_style( handle: 'ju_magnific_popup' );
-  wp_enqueue_style( handle: 'ju_responsive' );
-  wp_enqueue_style( handle: 'ju_custom' );
-  */
+  wp_register_style( 'ju_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css');
+  wp_register_style( 'ju_style', get_template_directory_uri() . '/assets/style.css');
+  wp_register_style( 'ju_dark', get_template_directory_uri() . '/assets/css/dark.css');
+  wp_register_style( 'ju_font_icons', get_template_directory_uri() . '/assets/css/font-icons.css');
+  wp_register_style( 'ju_animate', get_template_directory_uri() . '/assets/css/animate.css');
+  wp_register_style( 'ju_magnific_popup', get_template_directory_uri() . '/assets/css/magnific-popup.css');
+  wp_register_style( 'ju_responsive', get_template_directory_uri() . '/assets/css/responsive.css');
+  wp_register_style( 'ju_custom', get_template_directory_uri() . '/assets/css/custom.css');
+  
+  wp_enqueue_style( 'ju_google_fonts' );
+  wp_enqueue_style( 'ju_bootstrap' );
+  wp_enqueue_style( 'ju_style' );
+  wp_enqueue_style( 'ju_dark' );
+  wp_enqueue_style( 'ju_font_icons' );
+  wp_enqueue_style( 'ju_animate' );
+  wp_enqueue_style( 'ju_magnific_popup' );
+  wp_enqueue_style( 'ju_responsive' );
+  wp_enqueue_style( 'ju_custom' );
+  
 }
 
 ?>
