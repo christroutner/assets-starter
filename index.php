23fe43fc2c63174538a5c14152ea392cbdfb987b
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 16:29:53 -0700

    Adding javascript scripts

--- index.php
+++ index.php
@@ -450,14 +450,11 @@
 <div id="gotoTop" class="icon-angle-up"></div>
 
 <!-- External JavaScripts
-============================================= -->
+============================================= 
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/plugins.js"></script>
-
-<!-- Footer Scripts
-============================================= -->
 <script type="text/javascript" src="js/functions.js"></script>
-
+-->
   <?php wp_footer(); ?>
   
 </body>
