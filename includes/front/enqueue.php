fedb5a2ad3ae4c3bae1a11b013ab4d1aadca751c
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 16:18:42 -0700

    Adding stylesheet php

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,7 +2,7 @@
 
 function ju_enqueue() {
   
-  wp_register_style( 'handle': 'ju_google_fonts', 'src': 'http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic' );
+  wp_register_style( handle: 'ju_google_fonts' );
   /*
   wp_register_style( handle: 'ju_bootstrap', src: get_template_directory_uri() . '/assets/css/bootstrap.css');
   wp_register_style( handle: 'ju_style', src: get_template_directory_uri() . '/assets/style.css');
