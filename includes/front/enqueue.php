790a249fafc77ce6fbe0e0526ae7a329188dca56
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 16:13:15 -0700

    Adding stylesheet php

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,14 +2,14 @@
 
 function ju_enqueue() {
   wp_register_style( handle: 'ju_google_fonts', src: . 'http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic' );
-  wp_register_style( handle: 'ju_bootstrap' src: get_template_directory_uri() . '/assets/css/bootstrap.css');
-  wp_register_style( handle: 'ju_style' src: get_template_directory_uri() . '/assets/style.css');
-  wp_register_style( handle: 'ju_dark' src: get_template_directory_uri() . '/assets/css/dark.css');
-  wp_register_style( handle: 'ju_font_icons' src: get_template_directory_uri() . '/assets/css/font-icons.css');
-  wp_register_style( handle: 'ju_animate' src: get_template_directory_uri() . '/assets/css/animate.css');
-  wp_register_style( handle: 'ju_magnific_popup' src: get_template_directory_uri() . '/assets/css/magnific-popup.css');
-  wp_register_style( handle: 'ju_responsive' src: get_template_directory_uri() . '/assets/css/responsive.css');
-  wp_register_style( handle: 'ju_custom' src: get_template_directory_uri() . '/assets/css/custom.css');
+  wp_register_style( handle: 'ju_bootstrap', src: get_template_directory_uri() . '/assets/css/bootstrap.css');
+  wp_register_style( handle: 'ju_style', src: get_template_directory_uri() . '/assets/style.css');
+  wp_register_style( handle: 'ju_dark', src: get_template_directory_uri() . '/assets/css/dark.css');
+  wp_register_style( handle: 'ju_font_icons', src: get_template_directory_uri() . '/assets/css/font-icons.css');
+  wp_register_style( handle: 'ju_animate', src: get_template_directory_uri() . '/assets/css/animate.css');
+  wp_register_style( handle: 'ju_magnific_popup', src: get_template_directory_uri() . '/assets/css/magnific-popup.css');
+  wp_register_style( handle: 'ju_responsive', src: get_template_directory_uri() . '/assets/css/responsive.css');
+  wp_register_style( handle: 'ju_custom', src: get_template_directory_uri() . '/assets/css/custom.css');
   
   wp_enqueue_style( handle: 'ju_google_fonts' );
   wp_enqueue_style( handle: 'ju_bootstrap' );
