c1c436bf5c4932113513fcadc7886208396503b9
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 21:08:25 -0700

    Fixing styles references

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -3,7 +3,7 @@
 function ju_enqueue() {
   
   wp_register_style( 'ju_google_fonts', 'http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic' );
-  wp_register_style( 'ju_bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+  wp_register_style( 'ju_bootstrap', get_template_directory_uri() . 'css/bootstrap.css');
   wp_register_style( 'ju_style', get_template_directory_uri() . '/style.css');
   wp_register_style( 'ju_dark', get_template_directory_uri() . '/css/dark.css');
   wp_register_style( 'ju_font_icons', get_template_directory_uri() . '/css/font-icons.css');
